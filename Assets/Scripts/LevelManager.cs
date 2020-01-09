d1a4f2731e2cb1168183ff2da87479272d8c04f2
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-01-08 23:30:34 -0300

    added LevelManager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+  [SerializeField]
+  private List<Level> listLevel;
+  private LevelState lvlState;
+  [SerializeField]
+  private GameObject button;
+  [SerializeField]
+  private Transform panelLevels;
+
+  void RenderListLevel() {
+    foreach (var level in listLevel) {
+      GameObject newButton = Instantiate(button) as GameObject;
+
+      lvlState = newButton.GetComponent<LevelState>();
+      lvlState.setText(level.levelTxt);
+      lvlState.setButtonState(level.unblocked);
+
+      newButton.transform.SetParent(panelLevels, false);
+    }
+  }
+
+  void Start() {
+    RenderListLevel();
+  }
+
+}
+
+[System.Serializable]
+public class Level {
+  public string levelTxt;
+  public bool unblocked;
+  public int allowed;
+}
---
+++ Assets/Scripts/LevelState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelState : MonoBehaviour {
+  [SerializeField]
+  private Text txtState;
+  [SerializeField]
+  private Button btnState;
+
+  void Start() {
+    txtState = GetComponent<Text>();
+    btnState = GetComponent<Button>();
+  }
+
+  public void setText(string value) {
+    this.txtState.text = value;
+  }
+
+  public Text getText(string value) {
+    return this.txtState;
+  }
+
+  public void setButtonState(bool value) {
+    btnState.interactable = value;
+  }
+
+  public Button getButton(string value) {
+    return this.btnState;
+  }
+}
