b22a46004d6e32ac9f0fcadf993e52965fff90fa
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-02-07 02:36:40 -0300

    updated

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+   public static PlayerManager playerManager;
+
+   private void Awake() {
+      if (playerManager == null) {
+         playerManager = this;
+         DontDestroyOnLoad(gameObject);
+      }
+      else {
+         Destroy(gameObject);
+      }
+   }
+
+   void OnCollisionEnter2D(Collision2D other) {
+      if (other.gameObject.CompareTag("Obstacle")) {
+         AudioManager.audioManager.PlayClipFX(0);
+      }
+   }
+}
